adae56effa0c22466d8938762c22dcdf293c7c61
fjrotgerl <fjrotgerl@gmail.com>
2025-02-22 16:54:32 +0100

    Footer básico creado

---
+++ components/layout/MFooter.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet } from "react-native";
+import { MSeetingsIcon } from "@/components/misc/icons/MSettingsIcon";
+import { MUserIcon } from "@/components/misc/icons/MUserIcon";
+
+export const MFooter = () => {
+  return (
+    <View style={styles.footer}>
+      <View style={styles.button}>
+        <MSeetingsIcon />
+      </View>
+      <View style={styles.button}>
+        <MUserIcon />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    footer: {
+      flexDirection: "row",
+      gap: 20,
+      height: 90,
+      backgroundColor: "#222",
+      justifyContent: "space-evenly",
+    },
+    button: {
+      flexDirection: "row",
+      justifyContent: "center",
+      paddingTop: 10,
+      flex: 1,
+    }
+  });
+  
